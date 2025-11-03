9ca516ff55b0a79f98c50b4166875d0e79355877
tgonet <tanmingzhe.99@gmail.com>
2025-11-03 19:56:47 +0800

    Fix import issue

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -2,8 +2,8 @@ package seedu.address.storage;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
